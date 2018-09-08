0688a7a11ff10edce5664cff696a68a67c8d2bbd
Morrianton1985 <morrianton@gmail.com>
2018-09-08 17:43:03 -0600

    created meal service

---
+++ MealPlanner/src/app/meal.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Meal } from './meal';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MealService {
+
+  constructor(private http: Http) {  }
+
+  // retrieve meals
+  getMeals()
+  {
+    return this.http.get('http://localhost:3000/api/meals')
+      .pipe(map(res => res.json()));  
+  }
+
+  // add meal
+  addMeal(newMeal)
+  {
+    let headers = new Headers();
+
+    headers.append('Content-type', 'application/json');
+    return this.http.post('http://localhost:3000/api/meal', newMeal, {headers:headers})
+      .pipe(map(res => res.json()));
+  }
+
+  // delete meal
+  deleteMeal(id)
+  {
+    return this.http.delete('http://localhost:3000/api/meal/' + id)
+      .pipe(map(res => res.json()));
+  }
+
+}
