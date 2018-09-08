c00616a99b22397229e946bbdeb8827c79249096
Morrianton1985 <morrianton@gmail.com>
2018-09-08 17:43:38 -0600

    created MealsComponent

---
+++ MealPlanner/src/app/meals/meals.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meals',
+  templateUrl: './meals.component.html',
+  styleUrls: ['./meals.component.css'],
+  providers: []
+})
+export class MealsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
