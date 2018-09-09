a01c2261d2685fb5fe971eb848de83291756ee15
Morrianton1985 <morrianton@gmail.com>
2018-09-09 11:36:09 -0600

    Added MealService to providers in decorator, imported MealService and Meal, and built out methods

--- MealPlanner/src/app/meals/meals.component.ts
+++ MealPlanner/src/app/meals/meals.component.ts
@@ -1,16 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { MealService } from '../meal.service';
+import { Meal } from '../meal';
 
 @Component({
   selector: 'app-meals',
   templateUrl: './meals.component.html',
   styleUrls: ['./meals.component.css'],
-  providers: []
+  providers: [MealService]
 })
 export class MealsComponent implements OnInit {
+meals: Meal[];
+meal: Meal;
+entree: string;
+side: string;
+drink: string;
 
-  constructor() { }
+  constructor(private mealService: MealService) { }
+
+  addMeal() {
+    const NEW_MEAL = {
+      entree: this.entree,
+      side: this.side,
+      drink: this.drink
+    }
+    this.mealService.addMeal(NEW_MEAL)
+    .subscribe(meal => {
+      this.meals.push(meal);
+      this.mealService.getMeals()
+      .subscribe(meals => 
+        this.meals = meals);
+    })
+  }
+
+  deleteMeal(id:any)
+  {
+    let meals = this.meals;
+
+    this.mealService.deleteMeal(id)
+    .subscribe(data =>{
+      if(data.n == 1)
+      {
+        for(let i = 0; i < meals.length; i++)
+        {
+          if(meals[i]._id == id)
+          {
+            meals.splice(i, 1);
+          }
+        }
+      }
+    });
+  }
 
   ngOnInit() {
+    this.mealService.getMeals()
+      .subscribe(meals => 
+        this.meals = meals);
+    
   }
 
 }
